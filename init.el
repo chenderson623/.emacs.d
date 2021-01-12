7107e93b2f6d085b4c6849606ce1fe212e6f1f43
Chris Henderson <chris_henderson@swifttrans.com>
2021-01-12 09:26:11 -0700

    update configs

--- init.el
+++ init.el
@@ -16,3 +16,23 @@
     (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("b5fff23b86b3fd2dd2cc86aa3b27ee91513adaefeaa75adc8af35a45ffb6c499" default))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2B34" "#FAC863"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2B34" "#99C794"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2B34" "#A7ADBA"))
+ '(objed-cursor-color "#EC5f67")
+ '(pdf-view-midnight-colors (cons "#D8DEE9" "#1B2B34"))
+ '(rustic-ansi-faces
+   ["#1B2B34" "#EC5f67" "#99C794" "#FAC863" "#6699CC" "#E27E8D" "#5FB3B3" "#D8DEE9"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
